a470c336f0e28b21280da17e0e5c7470ff465b32
Jeremy Long <jeremy.long@gmail.com>
2025-08-15 08:02:21 -0400

    chore: use latest client

--- update_cache.sh
+++ update_cache.sh
@@ -1,21 +1,21 @@
 #/bin/bash
 
-#git checkout main
-#git pull
+git checkout main
+git pull
 git checkout gh-pages
 git pull origin gh-pages
 
-curl https://github.com/jeremylong/Open-Vulnerability-Project/releases/download/v7.0.1/vulnz-7.0.1.jar -s -L -o vulnz.jar
+curl https://github.com/jeremylong/Open-Vulnerability-Project/releases/download/v9.0.0/vulnz-9.0.0.jar -s -L -o vulnz.jar
 git reset --soft HEAD~1
 
-rm -rf ./nvd_cache
+mkdir nvd_cache
 
 ls -la .
 
 echo "login to 1password"
 eval $(op signin)
 
-op run --java -jar vulnz.jar cve --cache --directory ./nvd_cache  --threads=4 --maxRetry=40 --debug --delay=8000
+op run --java -jar vulnz.jar cve --cache --directory ./nvd_cache --delay=8000
 rm vulnz.jar
 git status
 cat nvd_cache/cache.properties
